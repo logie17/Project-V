1f978c10fbfa2f6636406abe3a5936e43db5dcd0
Kevin Mulvey <kmulvey@linux.com>
2015-01-13 17:10:05 -0500

    param order matters and need to bind to unused port

--- src/hangouts/server.go
+++ src/hangouts/server.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 )
 
-func main(){
-	http.HandleFunc("/", indexHandler);
-	http.ListenAndServe(":80", nil)
-}
-
-func indexHandler(r *http.Request, w http.ResponseWriter){
+func indexHandler(w http.ResponseWriter, r *http.Request){
 	// screw you vishal, (he doesnt want me to use a framework)
 	fmt.Fprintf(w, "<h1>%s</h1>", "Logie Sucks")
 }
+func main(){
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":1234", nil)
+}
+

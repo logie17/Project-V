57f2c686323926ff0d572f68b3e355e89f0fd246
Kevin Mulvey <kmulvey@linux.com>
2015-01-14 20:59:32 -0500

    comments

--- src/hangouts/routes.go
+++ src/hangouts/routes.go
@@ -7,6 +7,7 @@ import (
 
 var index_tmpl = pongo2.Must(pongo2.FromFile("templates/index.html"))
 
+// TODO: test this sucker and figure out how to mock net/http
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	err := index_tmpl.ExecuteWriter(pongo2.Context{"query": r.FormValue("query")}, w)
 	if err != nil {

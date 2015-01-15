b413f202f579c64fed11ecceaaff41a200da44fe
Kevin Mulvey <kmulvey@linux.com>
2015-01-14 19:43:05 -0500

    woops

--- src/hangouts/routes.go
+++ src/hangouts/routes.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/flosch/pongo2"
 	"net/http"
 )
 
 var index_tmpl = pongo2.Must(pongo2.FromFile("templates/index.html"))
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>%s</h1>", "Logie Sucks")
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	err := index_tmpl.ExecuteWriter(pongo2.Context{"query": r.FormValue("query")}, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
--- src/hangouts/server.go
+++ src/hangouts/server.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", IndexHandler)
+	r.HandleFunc("/", indexHandler)
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 	http.Handle("/", r)
 	http.ListenAndServe(":8100", nil)

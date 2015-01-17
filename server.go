930a73848b54ebf02446e0b7b23b1469194a6382
Logan Bell <loganbell@gmail.com>
2015-01-16 23:51:59 -0800

    We now have a json config file

--- server.go
+++ server.go
@@ -3,13 +3,16 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"net/http"
+	"github.com/logie17/Project-V/config"
+        "fmt"
 )
 
 func main() {
 	r := mux.NewRouter()
-
+	configuration := config.LoadConfig()
 	r.HandleFunc("/", indexHandler)
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
 	http.Handle("/", r)
-	http.ListenAndServe(":8100", nil)
+	fmt.Printf("Starting on Port: %v\n", configuration.Port)
+	http.ListenAndServe(fmt.Sprintf(":%s", configuration.Port), nil)
 }

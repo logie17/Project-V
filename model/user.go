df66a97025abadfa0af0338bd4f2293ccdb83494
Logan Bell <loganbell@gmail.com>
2015-01-18 16:52:46 -0800

    Modified to import appropriate libs

--- model/user.go
+++ model/user.go
@@ -1,8 +1,12 @@
 package model
 
+import (
+	"time"
+)
+
 type User struct {
 	Id           int64
-	Username     sql.NullString `sql:not null`
+	Username     string `sql:"not null`
 	Name         string  `sql:"size:255"`
 	CreatedAt    time.Time
 	UpdatedAt    time.Time

f72a45a23c213a0c3104fcd36e93bab0ffd59aa0
Kevin Mulvey <kmulvey@linux.com>
2015-01-22 08:05:15 -0500

    popover

--- public/js/app/home.js
+++ public/js/app/home.js
@@ -1,10 +1,14 @@
-define(["jquery"], function($){
+define(["jquery", "swig", "bootstrap"], function($, swig){
+	function processTemplateAsString(id){
+		return swig.render(document.getElementById(id).innerHTML, { });
+	}
 	$("body").on("blur click dblclick focus", function(e){
 		console.log(e.type, e.target);
 	});
 	$(".right-pane .btn").popover({
-		content: "<ul class='list-unstyled'><li>Chat</li><li>Video</li><li>tmux</li></ul>",
+		content: processTemplateAsString("user-action-popover"),
 		html: true,
+		trigger: "focus",
 		template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>'
 	});
 });

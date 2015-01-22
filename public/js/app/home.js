820ab9884b612ca076e722fbb503e67b0fe447e8
Kevin Mulvey <kmulvey@linux.com>
2015-01-22 07:29:11 -0500

    popover

--- public/js/app/home.js
+++ public/js/app/home.js
@@ -1,5 +1,10 @@
 define(["jquery"], function($){
 	$("body").on("blur click dblclick focus", function(e){
 		console.log(e.type, e.target);
-	});	
+	});
+	$(".right-pane .btn").popover({
+		content: "<ul class='list-unstyled'><li>Chat</li><li>Video</li><li>tmux</li></ul>",
+		html: true,
+		template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>'
+	});
 });

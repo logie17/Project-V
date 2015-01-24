99b8be94e96a41ef4f010006daa29a693c2db616
Kevin Mulvey <kmulvey@linux.com>
2015-01-24 16:06:40 -0500

    isauth working

--- gin.go
+++ gin.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
+	m "github.com/logie17/Project-V/middleware"
 )
 
 func main() {
@@ -14,7 +15,7 @@ func main() {
 	router.GET("/", indexHandler)
 	router.GET("/login", loginGetHandler)
 	router.POST("/login", loginPostHandler)
-	router.GET("/pair", isAuthenticated, pairGetHandler)
+	router.GET("/pair", m.IsAuthenticated(store), pairGetHandler)
 	router.Run(":3001")
 }
 
@@ -24,3 +25,20 @@ type LoginForm struct {
 	User     string `form:"email" binding:"required"`
 	Password string `form:"password" binding:"required"`
 }
+
+/*
+func isAuthenticated(c *gin.Context) {
+	fmt.Println(reflect.TypeOf(store))
+	session, err := store.Get(c.Request, "flash-session")
+	if err != nil {
+		http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
+	}
+	var username = session.Values["username"]
+	if username == nil {
+		session.AddFlash("Login failed!", "message")
+		session.Save(c.Request, c.Writer)
+		c.Fail(http.StatusUnauthorized, errors.New("Unauthorized")) // idk why this is needed but it is
+		c.Redirect(http.StatusMovedPermanently, "/login")
+	}
+}
+*/

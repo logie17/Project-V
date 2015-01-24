c3365a0bc193d42f1c6137ad2092c78255cb6851
Kevin Mulvey <kmulvey@linux.com>
2015-01-24 16:20:22 -0500

    clean up

--- gin.go
+++ gin.go
@@ -25,20 +25,3 @@ type LoginForm struct {
 	User     string `form:"email" binding:"required"`
 	Password string `form:"password" binding:"required"`
 }
-
-/*
-func isAuthenticated(c *gin.Context) {
-	fmt.Println(reflect.TypeOf(store))
-	session, err := store.Get(c.Request, "flash-session")
-	if err != nil {
-		http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
-	}
-	var username = session.Values["username"]
-	if username == nil {
-		session.AddFlash("Login failed!", "message")
-		session.Save(c.Request, c.Writer)
-		c.Fail(http.StatusUnauthorized, errors.New("Unauthorized")) // idk why this is needed but it is
-		c.Redirect(http.StatusMovedPermanently, "/login")
-	}
-}
-*/

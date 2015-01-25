3911823993f2f0ebdce828330cada87cf6a6adbe
Kevin Mulvey <kmulvey@linux.com>
2015-01-24 22:03:59 -0500

    start global template data

--- gin.go
+++ gin.go
@@ -12,6 +12,11 @@ func main() {
 	router := gin.New()
 	configuration := config.LoadConfig()
 	router.Use(Logrus())
+	router.Use(IsMobile()) // this doesnt work yet
+	// this is how we can get global template data
+	//set := pongo2.NewSet("our web templates") // The idea behind sets is that you can create another set with other globals and configurations for mail templates or other kind of templates
+	//set.Globals["global_variable"] = "this is a test"
+	// https://github.com/flosch/pongo2/issues/35
 	router.HTMLRender = newPongoRender()
 	router.Static("/public", "./public")
 
---
+++ mobile.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/Shaked/gomobiledetect"
+	"github.com/flosch/pongo2"
+	"github.com/gin-gonic/gin"
+)
+
+func IsMobile() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		detect := mobiledetect.NewMobileDetect(c.Request, nil)
+		if detect.IsMobile() {
+			var ctx map[string]interface{} = pongo2.Context{
+				"mobile": true,
+			}
+			c.Set("template_data", ctx)
+		}
+		c.Next()
+	}
+}

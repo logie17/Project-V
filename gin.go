61cee4615e16323a32ab8a8a00d9ad4a606bf4f1
Kevin Mulvey <kmulvey@linux.com>
2015-01-24 20:39:52 -0500

    config

--- gin.go
+++ gin.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
+	"github.com/logie17/Project-V/config"
 	m "github.com/logie17/Project-V/middleware"
 )
 
 func main() {
 	router := gin.New()
+	configuration := config.LoadConfig()
 	router.Use(Logrus())
 	router.HTMLRender = newPongoRender()
 	router.Static("/public", "./public")
@@ -16,7 +19,7 @@ func main() {
 	router.GET("/login", loginGetHandler)
 	router.POST("/login", loginPostHandler)
 	router.GET("/pair", m.IsAuthenticated(store), pairGetHandler)
-	router.Run(":3001")
+	router.Run(fmt.Sprintf("[::]:%s", configuration.Port))
 }
 
 var store *sessions.CookieStore = sessions.NewCookieStore([]byte("a-secret-string"))

08b88074b16bb6a76882354184b6d5c45a8f128d
Kevin Mulvey <kmulvey@linux.com>
2015-01-26 08:19:50 -0500

    start passwd storage

--- handles/signup.go
+++ handles/signup.go
@@ -1,12 +1,15 @@
 package handles
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/Sirupsen/logrus"
+	"github.com/agnivade/easy-scrypt"
 	"github.com/flosch/pongo2"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/gorilla/sessions"
-	"net/http"
 )
 
 type SignupForm struct {
@@ -32,12 +35,17 @@ func SignupPostHandler(store *sessions.CookieStore) gin.HandlerFunc {
 		// lets checkout the form
 		var form SignupForm
 		c.BindWith(&form, binding.Form)
+		key, err := scrypt.DerivePassphrase(form.Password, 32)
+		if err != nil {
+			fmt.Errorf("Error returned: %s\n", err)
+		}
 		logrus.WithFields(logrus.Fields{
 			"FirstName":    form.FirstName,
 			"LastName":     form.LastName,
 			"Organization": form.Organization,
 			"Email":        form.Email,
 			"Password":     form.Password,
+			"Hash":         key,
 		}).Info("User signup")
 		// logie should do something about this
 		ctx := pongo2.Context{
